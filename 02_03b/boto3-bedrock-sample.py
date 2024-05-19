ed143310edf2e39e847349c1027d780c5c6d967f
MidhunSMukundan <103759733+MidhunSMukundan@users.noreply.github.com>
2024-05-19 12:16:15 +0400

    Update boto3-bedrock-sample.py

--- 02_03b/boto3-bedrock-sample.py
+++ 02_03b/boto3-bedrock-sample.py
@@ -11,3 +11,37 @@
 #Invoke the model
 
 #Parsing and displaying the output
+
+
+#Imports
+import boto3
+import json
+#Create the bedrock client
+bedrock = boto3.client('bedtock-runtime')
+#Setting the prompt
+prompt_data = """Command: Write me a blof about coching employees as leader.
+Blog:
+"""
+#Model specification
+modelId = "amazon.titan-text-express-v1"
+accept = "application/json"
+contentType = "application/json"
+
+#Configuring parameters to invoke the model
+body = json.dumps({
+"inputText" : prompt_data,
+"textGenerationConfig" :{
+  "maxTokenCount" : 1000
+}
+
+})
+#Invoke the model
+
+response = bedrock.invoke_model(
+  body = body,modelId=modelId,accept=accept,contentType=contentType
+)
+#Parsing and displaying the output
+
+response_body = json.loads(response.get('body').read())
+output = response_body.get('result')[0].get("outputText")
+print(output)
